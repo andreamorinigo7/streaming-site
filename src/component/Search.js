718ff7660c3fb18dd28b41e4688181a2ac0394d6
Andrea <morinigoandrea7@gmail.com>
2025-01-03 12:49:44 -0500

    functional search button

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,12 +1,58 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const navigate = useNavigate();
+
+  function handleResponse(response) {
+    setResults(response.data.data);
+  }
+  function handleSearchResults(event) {
+    setQuery(event.target.value);
+  }
+
+  function handleError(error) {
+    console.error("Error fetching anime ...", error);
+  }
+
+  function handleSearch(event) {
+    event.preventDefault();
+
+    axios
+      .get(`https://api.jikan.moe/v4/anime?q=${query}`)
+      .then(handleResponse)
+      .catch(handleError);
+  }
+
+  function handleResultClick(id) {
+    navigate(`/anime/${id}`);
+  }
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Search for an anime..." />
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={query}
+          placeholder="Search for an anime..."
+          onChange={handleSearchResults}
+        />
       </form>
+      <div>
+        {results.map((anime) => (
+          <div
+            key={anime.mal_id}
+            onClick={() => handleResultClick(anime.mal_id)}
+            style={{ cursor: "pointer" }}
+          >
+            {anime.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
