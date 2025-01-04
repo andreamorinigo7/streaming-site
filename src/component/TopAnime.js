1055839471a4e81395de4c668052ba7350a84ff0
Andrea <morinigoandrea7@gmail.com>
2025-01-03 21:20:25 -0500

    update TopAnime with basic structure

--- src/component/TopAnime.js
+++ src/component/TopAnime.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import AnimeCard from "./AnimeCard";
+import axios from "axios";
 
 export default function TopAnime() {
+  const [topAnimes, setTopAnimes] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.jikan.moe/v4/top/anime")
+      .then((response) => setTopAnimes(response.data.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div>
-      <h1>hi from Top ANime</h1>
+      <h1>Top Anime</h1>
+      <div>
+        {topAnimes.map((anime) => (
+          <AnimeCard key={anime.mal_id} anime={anime} />
+        ))}
+      </div>
     </div>
   );
 }
