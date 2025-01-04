43a3f2e21f9e5decded98a88d18d719c745f4d21
Andrea <morinigoandrea7@gmail.com>
2025-01-03 21:15:32 -0500

    Update NowStreaming.js

--- src/component/NowStreaming.js
+++ src/component/NowStreaming.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import AnimeCard from "./AnimeCard";
 
 export default function NowStreaming() {
+  const [streaming, setStreaming] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.jikan.moe/v4/seasons/now")
+      .then((response) => setStreaming(response.data.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div>
-      <h1>hi from now streaming</h1>
+      <h1>Now Streaming</h1>
+      <div>
+        {streaming.map((anime) => (
+          <AnimeCard key={anime.mal_id} anime={anime} />
+        ))}
+      </div>
     </div>
   );
 }
